77102a8f5e016255f395187f2673d12865bb9308
Ryan Dowdy <rdowdy21@gmail.com>
2016-08-10 10:57:46 -0700

    Make table look nicer. Use ng-class on td. Set default priority. Tweak sortBy. Fix To-Do input label.

--- src/js/home.controller.js
+++ src/js/home.controller.js
@@ -15,17 +15,27 @@ angular.module('TodoApp').controller('HomeCtrl', function($scope, $filter) {
             num: 3
         }
     };
+    // default 
+    $scope.item = {};
+    $scope.item.priority = $scope.priorities.High;
 
     // add todo item to the todo list
     // then reset the input fields
     $scope.addTodo = function() {
-        $scope.list.push($scope.item);
-        $scope.item = {};
+        if($scope.item.text.length > 0) {
+            $scope.list.push($scope.item);
+            // default 
+            $scope.item = {};
+            $scope.item.priority = $scope.priorities.High;
+        }
     };
 
     // sort the todo list by the given expression
     $scope.sortBy = function(expression, reverse) {
-        $scope.list = $filter('orderBy')($scope.list, expression, reverse);
+        // sort first by the expression, then alphabetically
+        // this ensures consistent results
+        var expressions = [expression, 'text'];
+        $scope.list = $filter('orderBy')($scope.list, expressions, reverse);
     };
 
 });
