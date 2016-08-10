cd401887c6b68ef6f7fe55450ac23cabf85fc247
Ryan Dowdy <rdowdy21@gmail.com>
2016-08-09 21:37:20 -0700

    Add Basic ToDo list functionality. Start implementing list sorting

---
+++ src/js/form.controller.js
@@ -0,0 +1,3 @@
+angular.module('TodoApp').controller('FormCtrl', function($scope) {
+
+});
--- src/js/home.controller.js
+++ src/js/home.controller.js
@@ -1,3 +1,25 @@
 angular.module('TodoApp').controller('HomeCtrl', function($scope) {
+    $scope.list = [];
+    $scope.priorities = [
+        {
+            title: "High",
+            class: "high",
+            num: 1
+        },
+        {
+            title: "Medium",
+            class: "medium",
+            num: 2
+        },
+        {
+            title: "Low",
+            class: "low",
+            num: 3
+        }
+    ];
 
+    $scope.addTodo = function() {
+        $scope.list.push($scope.item);
+        $scope.item = {};
+    };
 });
