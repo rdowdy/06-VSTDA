1287ee9f825d28c80c36dff4d2214c5a37049974
Ryan Dowdy <rdowdy21@gmail.com>
2016-08-29 11:51:58 -0700

    Update todo list factory to reflect API changes.

--- src/js/todolist.factory.js
+++ src/js/todolist.factory.js
@@ -1,17 +1,17 @@
 angular.module('TodoApp').factory("todoListFactory", function($http, $filter) {
-    var port = "58048";
+    var url = "http://localhost:58048/api/todos";
     return {
         addTodoToList: function(item) {
-            return $http.post("http://localhost:" + port + "/api/todo", item);
+            return $http.post(url, item);
         },
         editTodo: function(changedTodo) {
-            return $http.put("http://localhost:" + port + "/api/todo", changedTodo);
+            return $http.put(url + "/" + changedTodo.todoId, changedTodo);
         },
         deleteTodo: function(toDelete) {
-            return $http.delete("http://localhost:" + port + "/api/todo", {"params" : toDelete});
+            return $http.delete(url + "/" + toDelete.todoId);
         },
         getTodoList: function() {
-            return $http.get("http://localhost:" + port + "/api/todo");
+            return $http.get(url);
         }
     };
 });
