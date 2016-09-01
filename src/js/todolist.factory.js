14cb32aa235229430fcb9d3d83a4bec07c0ac38b
Ryan Dowdy <rdowdy21@gmail.com>
2016-09-01 08:41:26 -0700

    Add default todo group for now.

--- src/js/todolist.factory.js
+++ src/js/todolist.factory.js
@@ -1,7 +1,9 @@
 angular.module('TodoApp').factory("todoListFactory", function($http, $filter) {
     var url = "http://localhost:58048/api/todos";
+    var defaultGroupId = 12;
     return {
         addTodoToList: function(item) {
+            item.todoGroupId = 12;
             return $http.post(url, item);
         },
         editTodo: function(changedTodo) {
