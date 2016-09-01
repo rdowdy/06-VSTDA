dbee82998a8f74569f158397b7454e2ef2056159
Ryan Dowdy <rdowdy21@gmail.com>
2016-09-01 08:55:11 -0700

    Merge conflict. Apply stashed changes.

--- src/js/todolist.factory.js
+++ src/js/todolist.factory.js
@@ -3,19 +3,28 @@ angular.module('TodoApp').factory("todoListFactory", function($http, $filter) {
     // this var is temporary until the list grouping
     // is set up on the front end
     var defaultGroupId = 12;
-    return {
-        addTodoToList: function(item) {
-            item.todoGroupId = 12;
-            return $http.post(url, item);
-        },
-        editTodo: function(changedTodo) {
-            return $http.put(url + "/" + changedTodo.todoId, changedTodo);
-        },
-        deleteTodo: function(toDelete) {
-            return $http.delete(url + "/" + toDelete.todoId);
-        },
-        getTodoList: function() {
-            return $http.get(url);
-        }
+    var service = {
+        addTodoToList: addTodoToList,
+        editTodo: editTodo,
+        deleteTodo: deleteTodo,
+        getTodoList: getTodoList
     };
+    return service;
+
+    function addTodoToList(item) {
+        item.todoGroupId = 12;
+        return $http.post(url, item);
+    }
+
+    function editTodo(changedTodo) {
+        return $http.put(url + "/" + changedTodo.todoId, changedTodo);
+    }
+
+    function deleteTodo(toDelete) {
+        return $http.delete(url + "/" + toDelete.todoId);
+    }
+
+    function getTodoList() {
+        return $http.get(url);
+    }
 });
